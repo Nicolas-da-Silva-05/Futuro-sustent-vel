33caaac2ffcefcf2ca3f647b6f52ea46c6e08829
Nicolas-da-Silva-05 <nicolas.da.silva05@escola.pr.gov.br>
2026-06-13 21:48:10 -0300

    Futuro sustentável
    Como intercalar a produção e meio ambiente

---
+++ script .js
@@ -0,0 +1,7 @@
+script.js
+6const botao = document.getElementById("botaoMensagem");
+
+botao.addEventListener("click", () => {
+    document.getElementById("mensagem").innerHTML =
+    "🌱 Cuidar do meio ambiente hoje é garantir um futuro melhor para todos!";
+});
